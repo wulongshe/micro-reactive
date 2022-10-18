8ed8cfc45bf796753b4c07e7428a0888ed97be82
shewulong <shewl@g-linkcloud.com>
2022-10-18 16:32:53 +0800

    feature: useEffect return value

--- tests/effect.spec.ts
+++ tests/effect.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from 'vitest'
 import { useEffect, track, trigger } from '../src/effect'
+import type { EffectFunction } from '../src/type'
 
 test('[effect]: useEffect, track, trigger, clear', async () => {
   let count = 0
 
-  const effects = new Set<() => void>()
+  const effects = new Set<EffectFunction>()
   useEffect(() => {
     count++
     track(effects)
