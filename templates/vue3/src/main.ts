7ab788057681e16577766344c8eab218f54dfac3
shewulong <shewl@g-linkcloud.com>
2022-10-18 11:41:25 +0800

    feature: vue3 template

---
+++ templates/vue3/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp, ReactiveEffect } from 'vue'
+import { useEffect } from 'micro-reactive'
+import './style.css'
+import App from './App.vue'
+
+// hack ReactiveEffect
+const hackRun = ReactiveEffect.prototype.run
+ReactiveEffect.prototype.run = function() {
+  useEffect(() => {
+    hackRun.call(this)
+  })
+}
+
+createApp(App).mount('#app')
