d616fa99bfd5be1fd74b1e6776343695212e8f8f
shewulong <shewulong@outlook.com>
2022-10-18 11:41:25 +0800

    feature: vue3 template

---
+++ templates/vue3/src/components/MicroCounter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useReactive } from 'micro-reactive'
+
+const count = useReactive(0)
+</script>
+
+<template>
+  <button type="button" @click="count(count() + 1)">count is {{ count() }}</button>
+</template>
