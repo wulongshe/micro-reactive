13fd501f3a1674580106f396909fd37688c7191c
shewulong <shewulong@outlook.com>
2022-10-28 16:16:48 +0800

    feat: module store template

---
+++ templates/store/src/main.ts
@@ -0,0 +1,25 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import { setupCounter } from './counter'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card" id="counter">
+      <button id="count" type="button"></button>
+      <div id="double"></div>
+      <div id="square"></div>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
