b8ba2bcc1956d86a6c9f23bc9f967b58b2f68ff8
shewulong <shewulong@outlook.com>
2022-10-31 15:04:37 +0800

    feat: update store template

--- templates/store/src/counter.ts
+++ templates/store/src/counter.ts
@@ -1,12 +1,12 @@
 import { useEffect } from 'micro-reactive'
-// import { count, double, increase, square } from './module'
-import { store } from './options'
-const { count, double, increase, square } = store
+// import { count, increase, message } from './module'
+// import { store } from './options'
+import { store } from './setup'
+const { count, increase, message } = store
 
 export function setupCounter(element: HTMLButtonElement) {
   element.addEventListener('click', increase)
 
   useEffect(() => element.innerHTML = `count is ${count()}`)
-  useEffect(() => console.log(`double is ${double()}`))
-  useEffect(() => console.log(`square is ${square()}`))
+  useEffect(() => console.log(message()))
 }
