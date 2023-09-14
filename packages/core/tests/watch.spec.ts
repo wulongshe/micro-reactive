fb2779b5af7a08d9f6c1a4a29031610daa66a290
shewulong <wulong.she397@imaygou.com>
2023-09-14 17:22:31 +0800

    feat(watch): 🚀 回调函数参数加old value

--- packages/core/tests/watch.spec.ts
+++ packages/core/tests/watch.spec.ts
@@ -4,21 +4,21 @@ import { watch } from '../src/watch'
 
 test('watch', async () => {
   const data1 = useReactive(1)
-  const data2 = useReactive(2)
+  const data2 = useReactive('2')
 
   const ret1: number[] = []
-  watch((value) => {
+  watch(([value]) => {
     ret1.push(value)
-  }, data1)
+  }, [data1])
 
-  const ret2: [number, number][] = []
-  watch((value1, value2) => {
-    ret2.push([value1, value2])
-  }, data1, data2)
+  const ret2: [number, string][] = []
+  watch((value1) => {
+    ret2.push(value1)
+  }, [data1, data2] as const)
 
   data1(4)
   expect(ret1).toEqual([1, 4])
 
-  data2(8)
-  expect(ret2).toEqual([[1, 2], [4, 2], [4, 8]])
+  data2('8')
+  expect(ret2).toEqual([[1, '2'], [4, '2'], [4, '8']])
 })
